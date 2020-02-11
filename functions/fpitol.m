db3c663572aeee8ee4bbaf1697d05319bc7d7077
toberge <toberge@stud.ntnu.no>
2020-02-11 12:48:14 +0100

    Solved 1.4-CP6 and improved verbose fpi function

--- functions/fpitol.m
+++ functions/fpitol.m
@@ -7,16 +7,23 @@ function xc=fpitol(g, x0, k, tol)
 x = zeros(k+1, 1);
 e = zeros(k+1, 1);
 x(1)=x0;
+xc = 'not set';
 % Perform the iteration
 for i=1:k
     % Calculate value with previous value as input
     x(i+1)=g(x(i));
     e(i+1)=abs(x(i+1)-x(i));
     if (e(i+1) < tol)
+        xc=x(i+1);
+        x = x(1:i+1);
+        e = e(1:i+1);
         break
     end
 end
 % Display result
 disp(table(x, e))
 % Return for good measure
-xc=x(k+1);
+if ~isnumeric(xc)
+    disp('Did not find a good estimate.')
+    xc=x(k+1);
+end
